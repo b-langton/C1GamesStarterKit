0b595eca240e5bb6cb0a693e181687119afcb71d
Adam Walker <adamwalker567123@gmail.com>
2020-02-29 15:59:50 -0800

    increases offensive size if stalemate

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -272,6 +272,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         num_spawns = int(bits // 5)
 
         reqd_spawns = 2 if game_state.get_resource(BITS, 1) >= 10 else 1
+        reqd_spawns = 4 if (game_state.turn_number > 30 and game_state.my_health > 25 and game_state.enemy_health > 25) else reqd_spawns
 
         if num_spawns >= 2:
             ping_locations = [[15, 1]]
