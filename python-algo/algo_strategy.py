da531fc9b98f75c808bdd9efc0667cee4c37bb80
Adam Walker <adamwalker567123@gmail.com>
2020-02-29 14:03:41 -0800

    fixup! scram on 1st turn

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -220,7 +220,7 @@ class AlgoStrategy(gamelib.AlgoCore):
             return
         
         if game_state.get_resource(BITS, 1) >= 15:
-            game_state.attempt_spawn(SCRAMBLER, [[8, 5]])
+            game_state.attempt_spawn(SCRAMBLER, [[6, 7]])
 
         encryptors_locations = [[5, 10], [6, 9], [7, 8], [8, 8], [8, 7], [9, 7], [9, 6], [10, 6], [10, 5], [11, 5], [11, 4], [12, 4]]
 
